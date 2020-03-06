a0d3f86dc8c5733e157b11715ab2753ed5db3c67
Tyron <tyronsamaroo@gmail.com>
2020-03-05 23:13:16 -0500

    fix?

--- stockapp/__init__.py
+++ stockapp/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager 
+from flask_heroku import Heroku
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'aa7b78e1935378061106cfbd12e774b3'
 app.config['SQLALCHEMY_DATABASE_URI'] = "DATABASE_URL"
 
 db = SQLAlchemy(app)
+heroku = Heroku(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
