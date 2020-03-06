06d88bbd025585d16f43a9b1a00dbfadd9784a1a
Tyron <tyronsamaroo@gmail.com>
2020-03-05 23:37:51 -0500

    SHOULD WORK NOW

--- stockapp/__init__.py
+++ stockapp/__init__.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager 
 from flask_heroku import Heroku
-
+import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'aa7b78e1935378061106cfbd12e774b3'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
 heroku = Heroku(app)
@@ -18,4 +18,3 @@ login_manager.login_message_category = 'info'
 
 #Avoid Circular Imports
 from stockapp import routes 
-
