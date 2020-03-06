a66d3d6aa5332b3ec31eecc63666dfb6ef3b7b94
Tyron <tyronsamaroo@gmail.com>
2020-03-05 23:25:47 -0500

    PL

--- stockapp/__init__.py
+++ stockapp/__init__.py
@@ -7,7 +7,7 @@ from flask_heroku import Heroku
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'aa7b78e1935378061106cfbd12e774b3'
-app.config['SQLALCHEMY_DATABASE_URI'] = "DATABASE_URL"
+
 
 db = SQLAlchemy(app)
 heroku = Heroku(app)
@@ -18,3 +18,4 @@ login_manager.login_message_category = 'info'
 
 #Avoid Circular Imports
 from stockapp import routes 
+
